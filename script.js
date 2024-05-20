1811355dd8be0e4db3ba6022f392ae836ce861f7
Jack Barlow <Jack.Barlow@assumption.vic.edu.au>
2024-05-20 13:19:28 +1000

    Adding Initial Files

---
+++ script.js
@@ -0,0 +1,63 @@
+// Variables
+const body = document.getElementById('particles-js');
+const warning = document.getElementById('warning');
+
+const canvas = document.getElementById('canvas');
+const rect = canvas.getBoundingClientRect();
+
+const dvd = new Image();
+dvd.src = 'images/dvd.png';
+
+var u = 1;
+var v = 1;
+var x = 0;
+var y = 0;
+const blockSize = 60;
+
+/** @type {CanvasRenderingContext2D} */
+const ctx = canvas.getContext('2d');
+
+// Events
+window.addEventListener('resize', checkWindow);
+
+// Setup
+checkWindow();
+
+const mainInterval = setInterval(main, 0);
+
+// Functions
+function checkWindow() {
+    if (window.innerWidth < rect.width) {
+        body.style.filter = 'blur(4px)';
+        warning.style.visibility = 'visible';
+        canvas.style.visibility = 'hidden'
+    } else {
+        body.style.filter = 'none';
+        warning.style.visibility = 'hidden';
+        canvas.style.visibility = 'visible'
+    }
+}
+
+function main() {
+    cls();
+
+    ctx.fillStyle = 'black';
+
+    if (x + (blockSize * 1.5) > rect.width || x < 0) {
+        u = -u;
+    }
+
+    if (y + blockSize > rect.height || y < 0) {
+        v = -v;
+    }
+
+    ctx.drawImage(dvd, x, y, blockSize * 1.5, blockSize)
+
+    x += u;
+    y += v;
+}
+
+function cls() {
+    ctx.fillStyle = '#d7d7e8';
+    ctx.fillRect(0, 0, rect.width, rect.height);
+}
